05b2cf549a477741cd60b856fd8a2250784a56fc
Joacoruarte <ruartejoaquin@gmail.com>
2022-05-13 11:22:47 -0300

    ShowModal

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,10 +34,14 @@ const Home = ({
   const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
 
-  if(loading) return null
+  if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b  lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home = Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -45,7 +49,7 @@ const Home = ({
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
         <Banner netflixOriginals={netflixOriginals} />
-        <section className='md:space-y-24'>
+        <section className="md:space-y-24">
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
@@ -56,7 +60,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {showModal && <Modal/>}
+      {showModal && <Modal />}
     </div>
   )
 }
